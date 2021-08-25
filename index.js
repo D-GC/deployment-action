c8d639cd53faf8d027d01a3a2164b85a0472945b
deepak-gc <arian911@gmail.com>
2021-08-25 05:44:22 +0530

    output on failures

---
+++ index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+
+const { createDeployment } = require('./src/deployments.service');
+
+try {
+  const token = core.getInput('token');
+  const repository = core.getInput('repository');
+  const ref = core.getInput('ref');
+  const environment = core.getInput('environment');
+  const task = core.getInput('task');
+  const requiredContexts = JSON.parse(core.getInput('required-contexts'));
+  const payload = core.getInput('payload');
+
+  const payload = {
+    ref,
+    environment,
+    payload: undefined,
+    auto_merge: false,
+    task,
+    required_contexts: requiredContexts,
+    payload
+  };
+
+  createDeployment(repository, payload, token)
+    .then(({ id }) => {
+      core.setOutput("deployment-id", id);
+    })
+    .catch((e) => {
+      core.setFailed(e.message);
+    });
+} catch (error) {
+  core.setFailed(error.message);
+}
